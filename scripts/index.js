67be51022e0b21f9c9c60aec01f966b07b7ee89e
Siberianvoyager <ilya.kos1993@gmail.com>
2024-03-21 03:25:11 +0100

    cardsready

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,42 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+// @todo: Темплейт карточки  
+const cardTemplate = document.querySelector('#card-template');
+
+// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+
+// @todo: Функция создания карточки
+function createCard(data, deleteCallback) { 
+    const card = cardTemplate.content.cloneNode(true).querySelector('.places__item');
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        deleteCallback(card);
+    });
+
+    return card;
+}
+
+// @todo: Функция удаления карточки
+function deleteCard(card) {
+    card.remove();
+}
+
+// @todo: Вывести карточки на страницу
+function renderCards(cardsArray) {
+    cardsArray.forEach(cardData => {
+        const cardElement = createCard(cardData, deleteCard);
+        placesList.appendChild(cardElement);
+    });
+}
+
+renderCards(initialCards);
+
